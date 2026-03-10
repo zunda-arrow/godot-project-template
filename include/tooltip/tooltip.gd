0b20cd300eae03f630fd155bb54b57dc043760d4
Zunda <mailme@jacobhaussman.online>
2026-03-10 01:18:43 -0400

    Start over, get single tooltip working with better system

--- include/tooltip/tooltip.gd
+++ include/tooltip/tooltip.gd
@@ -13,14 +13,16 @@ enum Direction {
 	DOWN,
 }
 
-@export var show_when_hovering_over: Node2D = null
+@export var show_when_hovering_over: Sprite2D = null
 @export var resource: TooltipResource
 @export var direction: Direction = Direction.LEFT
 
 @onready var tooltip_name = $Margins/PanelContainer/VBoxContainer/HBoxContainer/name
 @onready var tooltip_description = $Margins/PanelContainer/VBoxContainer/VBoxContainer/description
 @onready var icon = $Margins/PanelContainer/VBoxContainer/HBoxContainer/icon
-@onready var tooltip = $Margins/PanelContainer
+@onready var tooltip_panel= $Margins/PanelContainer
+@onready var tooltip_root = $Margins
+
 
 var displayed_hints: Array[Node] = []
 
@@ -50,77 +52,101 @@ func _process(_delta: float):
 	var parent_top_left = parent_position + parent_size.position
 	var parent_bottom_right = parent_position + parent_size.end
 
-	position_tooltip()
+	position_main_tooltip()
 
 	var mouse_pos = get_viewport().get_mouse_position()
 	if mouse_pos.x > parent_top_left.x and mouse_pos.y > parent_top_left.y and mouse_pos.x < parent_bottom_right.x and mouse_pos.y < parent_bottom_right.y:
-		tooltip.show()
+		tooltip_panel.show()
 	else:
-		tooltip.hide()
+		tooltip_panel.hide()
 
-func position_tooltip():
+func position_main_tooltip():
 	var parent_position: Vector2 = show_when_hovering_over.position
 	var parent_size: Rect2 = show_when_hovering_over.get_rect()
+	var parent_rect = Rect2(parent_position + parent_size.position, parent_size.size)
 
-	var parent_top_left = parent_position + parent_size.position
-	var parent_bottom_right = parent_position + parent_size.end
-
-	var tooltip_size = get_tooltip_size()
-
-	var center_horizontal = parent_top_left.x + ((parent_size.end.x - parent_size.position.x) / 2)
-	var center_vertical = parent_top_left.y + ((parent_size.end.y - parent_size.position.y) / 2)
-
+	var direction_priority_list: Array[Direction] = []
 	if direction == Direction.LEFT:
-		position_left(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, false)
+		direction_priority_list = [Direction.LEFT, Direction.RIGHT]
 	if direction == Direction.RIGHT:
-		position_right(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, false)
+		direction_priority_list = [Direction.RIGHT, Direction.LEFT]
 	if direction == Direction.UP:
-		position_top(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, false)
+		direction_priority_list = [Direction.UP, Direction.DOWN]
 	if direction == Direction.DOWN:
-		position_bottom(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, false)
+		direction_priority_list = [Direction.DOWN, Direction.UP]
 
-func position_left(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, fail_if_repeat):
-	if (parent_top_left.x - tooltip_size.x < 0):
-		if fail_if_repeat:
-			return
-		position_right(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, true)
-		return
-	tooltip.global_position.x = parent_top_left.x - tooltip_size.x
-	tooltip.global_position.y = parent_top_left.y
+	tooltip_root.global_position = position_single_tooltip(get_tooltip_size(), parent_rect, direction_priority_list)
 
-func position_right(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, fail_if_repeat):
-	var screen_size = get_viewport().get_visible_rect().size
+func position_single_tooltip(tooltip, around, direction_priority: Array[Direction]):
+	for d in direction_priority:
+		var location = null
+		if d == Direction.LEFT:
+			location = try_place_left(tooltip, around)
+		if d == Direction.RIGHT:
+			location = try_place_right(tooltip, around)
+		if d == Direction.UP:
+			location = try_place_up(tooltip, around)
+		if d == Direction.DOWN:
+			location = try_place_down(tooltip, around)
+		if location != null:
+			return location
 
-	if (parent_bottom_right.x + tooltip_size.x > screen_size.x):
-		if fail_if_repeat:
-			return
-		position_left(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, true)
-		return
-	tooltip.global_position.x = parent_bottom_right.x
-	tooltip.global_position.y = parent_top_left.y
+	return Vector2(0, 0)
 
-func position_top(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, fail_if_repeat):
-	if (parent_top_left.y - tooltip_size.y < 0):
-		if fail_if_repeat:
-			return
-		position_bottom(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, true)
-		return
 
-	tooltip.global_position.x = center_horizontal - tooltip_size.x / 2
-	tooltip.global_position.y = parent_top_left.y - tooltip_size.y
+func try_place_left(tooltip: Vector2, around: Rect2):
+	var top_left_x = around.position.x - tooltip.x
+	var top_left_y = around.position.y + around.size.y / 2 - tooltip.y / 2
 
-func position_bottom(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, fail_if_repeat):
-	var screen_size = get_viewport().get_visible_rect().size
+	if top_left_x < 0:
+		return null
+	if top_left_y < 0:
+		top_left_y = 0
+	if top_left_y + tooltip.y >= get_viewport_rect().size.y:
+		top_left_y = get_viewport_rect().size.y - tooltip.y
 
-	if (parent_bottom_right.y + tooltip_size.y > screen_size.y):
-		if fail_if_repeat:
-			return
-		position_top(parent_top_left, parent_bottom_right, center_vertical, center_horizontal, tooltip_size, true)
-		return
-	tooltip.global_position.x = center_horizontal - tooltip_size.x / 2
-	tooltip.global_position.y = parent_bottom_right.y
+	return Vector2(top_left_x, top_left_y)
+
+func try_place_right(tooltip: Vector2, around: Rect2):
+	var top_left_x = around.position.x + around.size.x
+	var top_left_y = around.position.y + around.size.y / 2 - tooltip.y / 2
+
+	if top_left_x + tooltip.x > get_viewport_rect().size.x:
+		return null
+	if top_left_y < 0:
+		top_left_y = 0
+	if top_left_y + tooltip.y >= get_viewport_rect().size.y:
+		top_left_y = get_viewport_rect().size.y - tooltip.y
+
+	return Vector2(top_left_x, top_left_y)
+
+func try_place_up(tooltip: Vector2, around: Rect2):
+	var top_left_x = around.position.x + around.size.x / 2 - tooltip.x / 2
+	var top_left_y = around.position.y - tooltip.y
+
+	if top_left_y < 0:
+		return null
+	if top_left_x < 0:
+		top_left_x = 0
+	if top_left_x + tooltip.x >= get_viewport_rect().size.x:
+		top_left_x = get_viewport_rect().size.x - tooltip.x
+
+	return Vector2(top_left_x, top_left_y)
+
+func try_place_down(tooltip: Vector2, around: Rect2):
+	var top_left_x = around.position.x + around.size.x / 2 - tooltip.x / 2
+	var top_left_y = around.position.y + around.size.y 
+
+	if top_left_y + tooltip.y > get_viewport_rect().size.y:
+		return null
+	if top_left_x < 0:
+		top_left_x = 0
+	if top_left_x + tooltip.x >= get_viewport_rect().size.x:
+		top_left_x = get_viewport_rect().size.x - tooltip.x
+
+	return Vector2(top_left_x, top_left_y)
 
 func get_tooltip_size():
-	if tooltip:
-		return tooltip.size
+	if tooltip_panel:
+		return tooltip_panel.size
 	return Vector2(0.,0.)
